f1dea807d3054ad9d01eb16e3d2c4e4c5a28d5b7
Dragoz Morgado <dragozcm@gmail.com>
2020-10-21 11:13:40 -0500

    Primer commit

---
+++ contador/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return CupertinoApp(home: Text("holi"));
+  }
+}
